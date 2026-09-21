e251df6d74583b0f5664523166cc8dd43e8854a1
Nabila Yesmin <105479163+nabila555@users.noreply.github.com>
2026-09-21 23:35:27 +0600

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+
+       uint32_t rev = 0;
+
+        for (int i = 0; i < 32; i++) {
+
+            int bit = n % 2;
+
+            rev = rev * 2 + bit;
+
+            n = n / 2;
+        }
+
+        return rev;
+        
+    }
+};
